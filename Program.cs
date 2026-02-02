e29b2ea41662df04291cd2f633bb920b20b27296
Javier Rivera <ingjrz@gmail.com>
2026-02-01 18:41:41 -0600

    initial commit
    first commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,9 @@
-using System.Security.Claims;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddAuthentication("cookie")
-    .AddCookie("cookie");
 
 var app = builder.Build();
-
-
-app.MapGet("/login", () => Results.SignIn(
-    new ClaimsPrincipal(
-        new ClaimsIdentity(
-            new[] { new Claim("user_id", Guid.NewGuid().ToString()) },
-            "cookie"
-        )
-    ),
-    authenticationScheme: "cookie"
-    ));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
